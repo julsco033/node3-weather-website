d8cae1b4de55b9c6034f24780e6f27a346bd650e
JULS <juliusabnerco@gmail.com>
2021-06-26 09:38:44 +0800

    Setup for heroku

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw'
+    // const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/''.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw'
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=pk.eyJ1IjoianVsaXVzY28wMyIsImEiOiJja3E5N2R2ZnYwMHpnMm9vMm05YnY2bHJhIn0.hDFX4zffLLKLP7iro-hCmA"
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
